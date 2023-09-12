d994d85a9642078aae518eb1ca676434f655e95d
amir811537 <amirhossain.bc.75@gmail.com>
2023-09-12 15:58:57 +0600

    visited country add

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,26 +1,45 @@
 import React, { useEffect, useState } from "react";
 import Country from "./Country/Country";
 
+
 const Countries = () => {
   const [countries, setCountries] = useState([]);
+  const [visitedCountries, setvisitedCountries] = useState([]);
+
 
   useEffect(() => {
-    fetch('https://restcountries.com/v3.1/all')
-      .then(res => res.json())
-      .then(data => setCountries(data))
-      .catch(error => console.error("Error fetching data:", error));
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => setCountries(data))
+      .catch((error) => console.error("Error fetching data:", error));
   }, []);
+  
+  const handleVisitedCountries = (country) => {
+    console.log("add this to your visited country");
+   const newVisitedCountries =[...visitedCountries,country];
+   setvisitedCountries(newVisitedCountries);
+  };
 
   return (
-    <div style={{
-      display: "grid",
-      gridTemplateColumns: "repeat(3, 1fr)",
-      gap: "10px"
-    }}>
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(3, 1fr)",
+        gap: "10px",
+      }}
+    >
       <h3>Countries: {countries.length}</h3>
-      {
-        countries.map(country => <Country key={country.cca3} country={country}></Country>)
-      }
+      <h5>visited countries :{visitedCountries.length}</h5>
+      <ul>
+        {
+          visitedCountries.map(country=><li key={country.cca3} >{country.name.common}</li>)
+        }
+      </ul>
+      {countries.map((country) => (
+        <Country key={country.cca3} 
+        handleVisitedCountries={handleVisitedCountries}
+        country={country}></Country>
+      ))}
     </div>
   );
 };
--- src/components/Countries/Country/Country.jsx
+++ src/components/Countries/Country/Country.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react';
 import './Country.css'
-const Country = ({country}) => {
+const Country = ({country,handleVisitedCountries}) => {
 const {name,flags,population,area,cca3}=country;
 const [Visited,setVisited]=useState(false);
 const handeleVisited =()=>{
     setVisited(!Visited);
 }
+const passwithParams=()=>handleVisitedCountries(country);
+
 
     return (
-        <div className='country'>
+        <div  style={{ color:'black', backgroundColor:Visited?'purple':'red'}} className='country'>
           <div className='flex'>
           <h3>{name.common}</h3>
            <img src={flags.png} alt="" />
@@ -16,6 +18,7 @@ const handeleVisited =()=>{
           <p>population: {population}</p>
           <p>area: {area}  km²</p>
           <p><small>code : {cca3} </small></p>
+          <button onClick={()=>handleVisitedCountries(country)}>mark at visited</button>
           <button onClick={handeleVisited}>{Visited?'visited':'Going'}</button>
           {
             Visited?  '  i have visited this country':'  i wont to visited '
